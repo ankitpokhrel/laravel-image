04364fc04d88843894c67f6e0f97b5dee7b28da8
Ankit Pokhrel <ankitpokhrel@gmail.com>
2016-09-07 22:42:38 +0700

    Add test for image helper

--- tests/DataProvider.php
+++ tests/DataProvider.php
@@ -38,4 +38,32 @@ class DataProvider
             [800, 'text/plain'],
         ];
     }
+
+    /**
+     * @return array
+     */
+    public function imageOptions()
+    {
+        $default        = '<img src="http://localhost/uploads/contents/639bd5bc-3dec-4bbf-af19-201931d1d0c2/image.png?fit=crop-center&w=300&h=200" alt="" class="" />';
+        $withOptions    = '<img src="http://localhost/uploads/contents/639bd5bc-3dec-19d3-af19-201931d1d0c2/image.jpg?fit=crop-top-left&filt=sepia&w=800&h=600" alt="" class="" />';
+        $withAttributes = '<img src="http://localhost/uploads/contents/639bd5bc-34de-4bbf-af19-201931d1d0c2/image.jpeg?fit=crop-top-right&filt=sepia&w=150&h=80" alt="Alt text of an image" class="custom-class" />';
+
+        return [
+            ['uploads/contents/639bd5bc-3dec-4bbf-af19-201931d1d0c2/', 'image.png', 300, 200, [], [], $default],
+            [
+                'uploads/contents/639bd5bc-3dec-19d3-af19-201931d1d0c2/', 'image.jpg',
+                800, 600,
+                ['fit' => 'crop-top-left', 'filt' => 'sepia'],
+                [],
+                $withOptions,
+            ],
+            [
+                'uploads/contents/639bd5bc-34de-4bbf-af19-201931d1d0c2/', 'image.jpeg',
+                150, 80,
+                ['fit' => 'crop-top-right', 'filt' => 'sepia'],
+                ['alt' => 'Alt text of an image', 'class' => 'custom-class'],
+                $withAttributes,
+            ],
+        ];
+    }
 }
